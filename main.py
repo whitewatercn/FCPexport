16418bc91d1667e059cabe5248fbbefade2ada10
whitewatercn <whitewatercn@qq.com>
2023-06-23 11:07:17 +0800

    Update main.py
    增加了导出时间轴txt文件的功能

--- main.py
+++ main.py
@@ -13,16 +13,31 @@ def format_time(seconds):
     return '{:02d}:{:02d}'.format(int(minutes), int(seconds))
 
 
+FCPtimemarker = set()
+
 # 逐行读取文件，检索，打印
-with open(path, "r") as f:
-    for line in f:
+FCPtimemarker = set()  # 用于储存时间标记和注释的集合
+path = 'Info.fcpxml'
+with open(path, "r") as fcpxml:
+    for line in fcpxml:
         if "<chapter-marker start=\"" in line and "s\"" in line:
+            # 提取时间标记
             timestart = line.index("start=\"") + len("start=\"")
             timeend = line.index("s\"")
             timemarker = line[timestart:timeend]
             timemarker = format_time(fraction_to_float(timemarker))
+            # 提取注释
             commentstart = line.index("value=\"") + len("value=\"")
             commentend = line.index("\" posterOffset")
             comment = line[commentstart:commentend]
-            TimeMarkerComment =str(timemarker + ' ' + comment)
-            print(TimeMarkerComment)
+            TimeMarkerComment = str(timemarker + ' ' + comment)
+            # 添加到集合中
+            FCPtimemarker.add(TimeMarkerComment)
+            # 打印时间标记和注释
+            # print(TimeMarkerComment)
+
+# 将时间标记和注释保存到文件
+file_path = "FCPtimemarker.txt"
+with open(file_path, "w") as timemarkertxt:
+    for line in FCPtimemarker:
+        timemarkertxt.write(line + "\n")
