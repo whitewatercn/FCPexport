bc6904cb39f62284f8e268ba9aac437ba6c8ecd1
whitewatercn <whitewatercn@qq.com>
2023-06-24 21:49:44 +0800

    Update main.py
    修复一些bug（路径和字符串分割

--- main.py
+++ main.py
@@ -2,48 +2,15 @@
 path = './Info.fcpxml'
 
 # 时间从X/Y转化为X秒
-def fraction_to_float(fraction_string):
-    numerator, denominator = fraction_string.split('/')
-    return float(numerator) / float(denominator)
-
-# 时间从X秒转化为X分：Y秒
-def format_time(seconds):
-    minutes = seconds // 60
-    seconds = seconds % 60
-    return '{:02d}:{:02d}'.format(int(minutes), int(seconds))
-
-
-FCPtimemarker = set()
-
-# 逐行读取文件，检索，打印
-FCPtimemarker = set()  # 用于储存时间标记和注释的集合
-path = 'Info.fcpxml'
-with open(path, "r") as fcpxml:
-    for line in fcpxml:
-        if "<chapter-marker start=\"" in line and "s\"" in line:
-            # 提取时间标记
-            timestart = line.index("start=\"") + len("start=\"")
-            timeend = line.index("s\"")
-            timemarker = line[timestart:timeend]
-            timemarker = format_time(fraction_to_float(timemarker))
-            # 提取注释
-            commentstart = line.index("value=\"") + len("value=\"")
-            commentend = line.index("\" posterOffset")
-            comment = line[commentstart:commentend]
-            TimeMarkerComment = str(timemarker + ' ' + comment)
-            # 添加到集合中
-            FCPtimemarker.add(TimeMarkerComment)
-            # 打印时间标记和注释
-            # print(TimeMarkerComment)
-
-# 将时间标记和注释保存到文件
-# 指定文件路径
-path = './Info.fcpxml'
-
 # 时间从X/Y转化为X秒
 def fraction_to_float(fraction_string):
-    numerator, denominator = fraction_string.split('/')
-    return float(numerator) / float(denominator)
+    if '/' in fraction_string:
+        numerator, denominator = fraction_string.split('/')
+        return float(numerator) / float(denominator)
+    else:
+        # 处理没有斜杠的情况，例如直接给定秒数的情况
+        return float(fraction_string)
+
 
 # 时间从X秒转化为X分：Y秒
 def format_time(seconds):
@@ -52,11 +19,8 @@ def format_time(seconds):
     return '{:02d}:{:02d}'.format(int(minutes), int(seconds))
 
 
-FCPtimemarker = set()
-
 # 逐行读取文件，检索，打印
 FCPtimemarker = set()  # 用于储存时间标记和注释的集合
-path = 'Info.fcpxml'
 with open(path, "r") as fcpxml:
     for line in fcpxml:
         if "<chapter-marker start=\"" in line and "s\"" in line:
@@ -64,22 +28,21 @@ with open(path, "r") as fcpxml:
             timestart = line.index("start=\"") + len("start=\"")
             timeend = line.index("s\"")
             timemarker = line[timestart:timeend]
-            timemarker = format_time(fraction_to_float(timemarker))
+            timemarker_time = format_time(fraction_to_float(timemarker))
             # 提取注释
             commentstart = line.index("value=\"") + len("value=\"")
             commentend = line.index("\" posterOffset")
             comment = line[commentstart:commentend]
-            TimeMarkerComment = str(timemarker + ' ' + comment)
+            TimeMarkerComment = str(timemarker_time + ' ' + comment)
             # 添加到集合中
             FCPtimemarker.add(TimeMarkerComment)
             # 打印时间标记和注释
             # print(TimeMarkerComment)
 
 # 将时间标记和注释保存到文件
-file_path = "FCPtimemarker.txt"
+file_path = "./FCPtimemarker.txt"
 with open(file_path, "w") as timemarkertxt:
     # 按照时间排序
     sorted_FCPtimemarker = sorted(FCPtimemarker, key=lambda x: x[:5])
     for line in sorted_FCPtimemarker:
         timemarkertxt.write(line + "\n")
-
